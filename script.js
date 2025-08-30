fb11cd83410047cc2ddd0f9be32d053d92565968
Godoy015 <reaperoverwatch02@gmail.com>
2025-08-30 16:21:18 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const invitationText = {
         title: "ESTAS CORDIALMENTE INVITADO/A",
         content: "Acompáñanos a celebrar los XV Años de:<br><span id='alondra-name'>Alondra Torres Godoy</span><br>En una noche de magia y encanto, donde el Gran Comedor de Hogwarts será nuestro telón de fondo.",
-        details: "Fecha: 27 de Diciembre del 2025 <br>Hora: A partir de las 18 Hrs<br>Lugar: [Lugar]<br>"
+        details: "Fecha: 27 de Diciembre del 2025 <br>Hora: A partir de las 18 Hrs<br>Lugar: Jardín de Eventos "Manzano" <br> https://maps.app.goo.gl/pzq88P254Us6VpDaA"
     };
 
     const partyDate = new Date("December 27, 2025 23:59:59").getTime();
